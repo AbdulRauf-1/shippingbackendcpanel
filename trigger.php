d3ce3b554b80c429149890dab520760d8de7926d
Abdul Rauf <sigidevelopers@gmail.com>
2024-12-02 10:48:09 +0500

    pm2

--- trigger.php
+++ trigger.php
@@ -4,7 +4,12 @@ $workingDir = '/home/theshippinghack/rauf.theshippinghack.com';
 
 // Set HOME environment variable and run commands
 $output = shell_exec("export HOME=/home/theshippinghack && cd $workingDir && npm install && npm install -g pm2 && pm2 start shipping.js --name rauf 2>&1");
-
-// Output the result
 echo nl2br($output);
+// Set HOME environment variable and run commands
+$output2 = shell_exec("export HOME=/home/theshippinghack && cd $workingDir && npm install -g pm2 2>&1");
+echo nl2br($output2);
+// Set HOME environment variable and run commands
+$output3 = shell_exec("export HOME=/home/theshippinghack && cd $workingDir && pm2 start shipping.js --name rauf 2>&1");
+echo nl2br($output3);
+
 ?>
