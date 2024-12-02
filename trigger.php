b3cc1c14865eac59eac2feb2811f7a392bbca892
Abdul Rauf <sigidevelopers@gmail.com>
2024-12-02 09:37:21 +0500

    shipping

--- trigger.php
+++ trigger.php
@@ -2,9 +2,20 @@
 // Path to your working directory
 $workingDir = '/home/theshippinghack/rauf.theshippinghack.com';
 
-// Set HOME environment variable and run commands
-$output = shell_exec("export HOME=/home/theshippinghack && cd $workingDir && npm install 2>&1");
+// Command to check if the PM2 process is already running
+$checkPm2Command = "export HOME=/home/theshippinghack && pm2 list | grep rauf";
 
+// Execute the command
+$processExists = shell_exec($checkPm2Command);
+
+// If the process exists, restart it; otherwise, create and start it
+if (strpos($processExists, 'rauf') !== false) {
+    // Restart the existing PM2 process
+    $output = shell_exec("export HOME=/home/theshippinghack && cd $workingDir && ~/node/bin/pm2 restart rauf 2>&1");
+} else {
+    // Create and start a new PM2 process
+    $output = shell_exec("export HOME=/home/theshippinghack && cd $workingDir && npm install && ~/node/bin/pm2 start theshippingh.js --name rauf 2>&1");
+}
 // Output the result
 echo nl2br($output);
 ?>
